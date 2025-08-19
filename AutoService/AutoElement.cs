dd653bbdd3af516f7a69b28e8f848b49eb9ab20a
Oleg <finkton23@gmail.com>
2025-08-19 23:08:59 +0500

    Update AutoElement.cs

--- AutoService/AutoElement.cs
+++ AutoService/AutoElement.cs
@@ -1,6 +1,16 @@
-﻿namespace AutoService;
-
-public class AutoElement
+namespace AutoService
 {
-        
+    public class AutoElement
+    {
+        public int elementID { get; set; }
+        public string elementName { get; }
+        public int elementPrice { get; }
+
+        public AutoElement(int id, int price, string name)
+        {
+            elementID = id;
+            elementPrice = price;
+            elementName = name;
+        }
+    }
 }
