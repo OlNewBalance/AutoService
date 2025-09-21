8a13fa2462fb65a26ca721dd518178a9178e2d70
Oleg <finkton23@gmail.com>
2025-09-21 20:58:42 +0500

    Update AutoService.cs

--- AutoService/AutoService.cs
+++ AutoService/AutoService.cs
@@ -2,25 +2,28 @@ namespace AutoService
 {
     public class AutoService
     {
-        Storage _storage;
-
-        public AutoService()
-        {
-            _storage = new Storage();
-        }
-        static Random random = new Random();
-        public int _balance = 100000;
-        private const int Penalty = 2500;
-        private const int WorkCost = 20000;
-        public const int AutoQueue = 10;
         private int _lastAutoId = 0;
+        private Random _random = new Random();
+        
+        public const int _penalty = 2500;
+        public const int _workCost = 20000;
+        public const int AutoQueue = 10;
+
+        Storage _storage;
+        public int Balance = 100000;
         public int ToAddRadio = 0;
         public int ToAddWheel = 0;
         public int ToAddEngine = 0;
         public int ToAddHeadlights = 0;
         public int ToAddCarbureator = 0;
 
-        public List<AutoElement> elementsStorage = new List<AutoElement>()
+        public AutoService()
+        {
+            _storage = new Storage();
+        }
+
+
+        public List<AutoElement> ElementsStorage = new List<AutoElement>()
         {
             new AutoElement(1, Auto.WheelPrice, "Колесо"),
             new AutoElement(2, Auto.EnginePrice, "Двигатель"),
@@ -36,107 +39,16 @@ namespace AutoService
 
         public Queue<Auto> QueueOfAuto = new Queue<Auto>();
 
-        public void AcceptCar(Auto car)
-        {
-            car.CheckMissingParts(_storage._parts);
-
-            if (car.IsEngineMissing)
-            {
-                var engine = elementsStorage.FirstOrDefault(e => e.elementID == Auto.EngineId);
-                if (engine != null)
-                {
-                    Console.WriteLine("+ У машины проблемы с двигателем!");
-                    elementsStorage.Remove(engine);
-                    _balance += Auto.EnginePrice + WorkCost;
-                    Console.WriteLine($"Двигатель заменён! +{Auto.EnginePrice + WorkCost} руб.");
-                }
-                else
-                {
-                    _balance -= Penalty;
-                    Console.WriteLine($"Нет двигателя на складе! Штраф {-Penalty} руб.");
-                }
-            }
-
-            if (car.IsWheelMissing)
-            {
-                Console.WriteLine("+ У машины проблемы с колесом!");
-                var wheel = elementsStorage.FirstOrDefault(e => e.elementID == Auto.WheelId);
-                if (wheel != null)
-                {
-                    elementsStorage.Remove(wheel);
-                    _balance += Auto.WheelPrice + WorkCost;
-                    Console.WriteLine($"Колесо заменено! +{Auto.WheelPrice + WorkCost} руб.");
-                }
-                else
-                {
-                    _balance -= Penalty;
-                    Console.WriteLine($"Нет колеса на складе! Штраф {-Penalty} руб.");
-                }
-            }
-
-            if (car.IsRadioMissing)
-            {
-                Console.WriteLine("+ У машины проблемы с радио!");
-                var radio = elementsStorage.FirstOrDefault(e => e.elementID == Auto.RadioId);
-                if (radio != null)
-                {
-                    elementsStorage.Remove(radio);
-                    _balance += Auto.RadioPrice + WorkCost;
-                    Console.WriteLine($"Радио заменено! +{Auto.RadioPrice + WorkCost} руб.");
-                }
-                else
-                {
-                    _balance -= Penalty;
-                    Console.WriteLine($"Нет радио на складе! Штраф {-Penalty} руб.");
-                }
-            }
-
-            if (car.IsCarbureatorMissing)
-            {
-                Console.WriteLine("+ У машины проблемы с карбюратором!");
-                var carbureator = elementsStorage.FirstOrDefault(e => e.elementID == Auto.CarbureatorId);
-                if (carbureator != null)
-                {
-                    elementsStorage.Remove(carbureator);
-                    _balance += Auto.CarbureatorPrice + WorkCost;
-                    Console.WriteLine($"Карбюратор заменён! +{Auto.CarbureatorPrice + WorkCost} руб.");
-                }
-                else
-                {
-                    _balance -= Penalty;
-                    Console.WriteLine($"Нет карбюратора на складе! Штраф {-Penalty} руб.");
-                }
-            }
-
-            if (car.IsHeadlightsMissing)
-            {
-                Console.WriteLine("+ У машины проблемы с фарами!");
-                var headlights = elementsStorage.FirstOrDefault(e => e.elementID == Auto.HeadlightsId);
-                if (headlights != null)
-                {
-                    elementsStorage.Remove(headlights);
-                    _balance += Auto.HeadlightsPrice + WorkCost;
-                    Console.WriteLine($"Фары замены! +{Auto.HeadlightsPrice + WorkCost} руб.");
-                }
-                else
-                {
-                    _balance -= Penalty;
-                    Console.WriteLine($"Нет фар на складе! Штраф {-Penalty} руб.");
-                }
-            }
-            // Аналогично для других деталей
-        }
-
         public void AutosRenderer(int countToAdd)
         {
             for (int i = 0; i < countToAdd; i++)
             {
                 var newAuto = new Auto(
-                    isEngineMissing: random.Next(2) == 1,
-                    isCarbureatorMissing: random.Next(2) == 1,
-                    isRadioMissing: random.Next(2) == 1,
-                    isWheelMissing: random.Next(2) == 1,
-                    isHeadlightsMissing: random.Next(2) == 1,
+                    isEngineMissing: _random.Next(2) == 1,
+                    isCarbureatorMissing: _random.Next(2) == 1,
+                    isRadioMissing: _random.Next(2) == 1,
+                    isWheelMissing: _random.Next(2) == 1,
+                    isHeadlightsMissing: _random.Next(2) == 1,
                     autoId: ++_lastAutoId // Теперь ID будут уникальными
                 );
 
@@ -183,25 +95,6 @@ namespace AutoService
                     var newElement = new AutoElement(5, Auto.CarbureatorPrice, "Карбюратор");
                 }
             }
-        }
-
-        public int ExpensesCalc(int addRadio, int addWheel, int addCarbureator, int addEngine,
-            int addHeadlights)
-        {
-            int RadioExpense = Auto.RadioPrice;
-            int WheelExpense = Auto.WheelPrice;
-            int EngineExpense = Auto.EnginePrice;
-            int CarbureatorExpense = Auto.CarbureatorPrice;
-            int HeadlightsExpense = Auto.HeadlightsPrice;
-
-            RadioExpense *= addRadio;
-            WheelExpense *= addWheel;
-            CarbureatorExpense *= addCarbureator;
-            EngineExpense *= addEngine;
-            HeadlightsExpense *= addHeadlights;
-
-            int expenses = RadioExpense + WheelExpense + CarbureatorExpense + EngineExpense + addHeadlights;
-            return expenses;
         }
     }
 }
